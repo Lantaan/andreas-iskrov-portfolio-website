92f45853e10854d87f6a26b0b9a51f0661990b99
Lantaan <69048952+Lantaan@users.noreply.github.com>
2022-01-14 17:09:34 +0100

    loading screen

---
+++ src/2dNoCollisionSim.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+
+const forceFactor = 1 / 10,
+    timeFactor = 4 / 6;
+
+
+class PhysicsObject {
+    posX: number;
+    posY: number;
+    attractorX: number;
+    attractorY: number;
+    velocityX: number = 0;
+    velocityY: number = 0;
+    radius: number = 3;
+
+
+    constructor(posX: number, posY: number, attractorX: number, attractorY: number, velocityX?: number, velocityY?: number) {
+        this.posX = posX;
+        this.posY = posY;
+        this.attractorX = attractorX;
+        this.attractorY = attractorY;
+        if (velocityX) this.velocityX = velocityX;
+        if (velocityY) this.velocityY = velocityY;
+    }
+
+
+    update() {
+        this.updateVelocity();
+        this.updatePositions();
+    }
+
+    updateVelocity() {
+        const dx = this.attractorX - this.posX,
+            dy = this.attractorY - this.posY;
+        const d = Math.sqrt(dx ** 2 + dy ** 2);
+        this.velocityX += dx * forceFactor * timeFactor / d;
+        this.velocityY += dy * forceFactor * timeFactor / d;
+    }
+    updatePositions() {
+        this.posX += this.velocityX * timeFactor;
+        this.posY += this.velocityY * timeFactor;
+    }
+}
+
+
+
+class OscillatorSim extends React.Component<any, any>{
+    scale: number = 5;
+    objectFirstState: { x: number, y: number, ax: number, ay: number, vx: number, vy: number } = { x: 50, y: 0, ax: 0, ay: 0, vx: 0, vy: 1 }
+    objects: PhysicsObject[] = [];
+    framesBetweenNewObjects: number = 68 / timeFactor;
+    newObjectInNFrames: number = 0;
+    finishedAdding: boolean = false;
+
+
+    constructor(props: any) {
+        super(props);//this.addStandartObject();
+    }
+
+
+    render(): React.ReactNode {
+        return <>
+            {this.objects.map(object => <span className="bg-sky-500 absolute rounded-full" style={{
+                left: object.posX * this.scale, top: object.posY * this.scale,
+                width: object.radius * 2 * this.scale, height: object.radius * 2 * this.scale
+            }}></span>)}
+        </>
+    }
+
+
+    componentDidMount() {
+        this.simulatePhysicsRecursive();
+    }
+
+    simulatePhysicsRecursive() {
+        requestAnimationFrame(this.simulatePhysicsRecursive.bind(this));
+
+        this.newObjectInNFrames--;
+        if (this.newObjectInNFrames < 1) {
+            if (this.objects.length <= 6) this.addStandartObject();
+            else this.finishedAdding = true;
+            this.newObjectInNFrames = this.framesBetweenNewObjects;
+        }
+
+        this.objects.forEach(object => object.update());
+        this.setState({});
+    }
+
+    addStandartObject() {
+        this.objects.push(
+            new PhysicsObject(
+                this.objectFirstState.x,
+                this.objectFirstState.y,
+                this.objectFirstState.ax,
+                this.objectFirstState.ay,
+                this.objectFirstState.vx,
+                this.objectFirstState.vy
+            )
+        );
+    }
+}
+
+
+export default OscillatorSim;
--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, { Suspense } from 'react';
+import OscillatorSim from "./2dNoCollisionSim";
 
 
-function App(){
-  const ThreeJSStuff = React.lazy(()=>import("./ThreeJSStuff"))
+const ThreeJSStuff = React.lazy(() => import("./ThreeJSStuff"));
 
-  return (<>
-    <Suspense fallback={<div>Loading...</div>}>
-      {<ThreeJSStuff />}
-    </Suspense>
-  </>);
+
+class App extends React.Component {
+  loadingSimRef = React.createRef();
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loadingFinished: false
+    };
+
+    this.setLoadingFinishedToTrueWhenSimHasFinished();
+  }
+
+
+  render() {
+    const loadingSim = <div className='relative left-1/2 top-1/2'>
+      <OscillatorSim ref={this.loadingSimRef} />
+    </div>
+
+    return (
+      <div className='h-screen w-screen bg-black overflow-hidden'>
+        {<Suspense fallback={loadingSim}>
+          {this.state.loadingFinished ? <ThreeJSStuff /> : loadingSim}
+        </Suspense>}
+      </div>
+    );
+  }
+
+
+
+  async setLoadingFinishedToTrueWhenSimHasFinished() {
+    while (!this.loadingSimRef.current) await new Promise(resolve => setTimeout(resolve, 1));
+    while (!this.loadingSimRef.current.finishedAdding) await new Promise(resolve => setTimeout(resolve, 1));
+    this.setState({ loadingFinished: true });
+  }
 }
 
 
